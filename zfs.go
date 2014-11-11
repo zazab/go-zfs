48a6c165ca7ff416506096680bea70c7ddfe20a1
Evgeny Persienko <e.persienko@office.ngs.ru>
2014-11-11 18:30:15 +0600

    Add sudo layer

--- zfs.go
+++ zfs.go
@@ -3,14 +3,34 @@ package zfs
 import "github.com/theairkit/runcmd"
 
 type Zfs struct {
-	runner runcmd.Runner
+	runner ZfsRunner
 }
 
-var std, _ = NewZfs(runcmd.NewLocalRunner())
+type ZfsRunner struct {
+	runcmd.Runner
+	sudo bool
+}
 
-func NewZfs(r runcmd.Runner, err error) (*Zfs, error) {
-	if err != nil {
-		return nil, err
+func (z ZfsRunner) Command(cmd string) (runcmd.CmdWorker, error) {
+	if z.sudo {
+		cmd = "sudo " + cmd
 	}
-	return &Zfs{r}, nil
+	return z.Runner.Command(cmd)
+}
+
+var std, _ = NewZfsLocal(false)
+
+func NewZfsLocal(sudo bool) (Zfs, error) {
+	runner, err := runcmd.NewLocalRunner()
+	return Zfs{ZfsRunner{runner, sudo}}, err
+}
+
+func NewZfs(runner runcmd.Runner, sudo bool) Zfs {
+	return Zfs{ZfsRunner{runner, sudo}}
+}
+
+func SetStdSudo(sudo bool) error {
+	var err error
+	std, err = NewZfsLocal(sudo)
+	return err
 }
